902e5b7a28bcd78b305f3dd27a9b6d31144aa2ab
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2016-11-01 23:05:45 +0900

    さいのうさんのおかげで！

--- app/controllers/list_timeline_controller.rb
+++ app/controllers/list_timeline_controller.rb
@@ -2,7 +2,21 @@ class ListTimelineController < ApplicationController
   def listtimeline
     #@clientを使えるようにする。
     @client = twitter_client
-    #クリックされたされたリストの名前を文字データにして@tlに入れる。
     @tl = params[:name]
+    #クリックされたされたリストの名前を文字データにして@tlに入れる。
+    @tweets = @client.list_timeline(@tl,:include_entities => true)
+    @tweets_with_image = pick_tweets_with_image
+  end
+
+  private
+
+  def pick_tweets_with_image
+    if (@tweets.is_a?(Array))
+      @tweets.select do |tweet|
+        !!(tweet.text =~ /https?:\/\/t\.co\/\w{10}/)
+      end
+    else
+      []
+    end
   end
 end

d46d5d4d13ea4d81d384c436fff5ca7f5a8b87fc
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2016-11-04 15:26:15 +0900

    RTを含むツイートの除外、ツイート800件取得に変更。

--- app/controllers/list_timeline_controller.rb
+++ app/controllers/list_timeline_controller.rb
@@ -4,7 +4,7 @@ class ListTimelineController < ApplicationController
     @client = twitter_client
     @tl = params[:name]
     #クリックされたされたリストの名前を文字データにして@tlに入れる。
-    @tweets = @client.list_timeline(@tl,count: 100,:include_entities => true)
+    @tweets = @client.list_timeline(@tl,count: 800,:include_entities => true)
     @tweets_with_image = pick_tweets_with_image
   end
 
@@ -13,7 +13,7 @@ class ListTimelineController < ApplicationController
   def pick_tweets_with_image
     if (@tweets.is_a?(Array))
       @tweets.select do |tweet|
-        !!(tweet.text =~ /https?:\/\/t\.co\/\w{10}/)
+        !!(tweet.text =~ /https?:\/\/t\.co\/\w{10}/) && !(tweet.text =~ /RT/)
       end
     else
       []

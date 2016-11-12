b691e13ff2a381c8b081ad699cc905818378b54d
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2016-11-12 13:11:49 +0900

    コメントで細かい説明

--- app/controllers/list_timeline_controller.rb
+++ app/controllers/list_timeline_controller.rb
@@ -2,10 +2,15 @@ class ListTimelineController < ApplicationController
   def listtimeline
     #@clientを使えるようにする。
     @client = twitter_client
+    #クリックされたされたリスト名を文字データにして@tlに入れる。
     @tl = params[:name]
-    #クリックされたされたリストの名前を文字データにして@tlに入れる。
-    @tweets = @client.list_timeline(@tl,count: 800,:include_entities => true)
-    @tweets_with_image = pick_tweets_with_image
+    #画像付きツイートだけ格納
+     @tweets = @client.list_timeline(@tl,count: 200)
+     # :include_entities => trueを付けなくても動く。
+     # @tweets = @client.list_timeline(@tl,count: 200,:include_entities => true)
+
+     #https?~,RT,判別後格納。
+     @tweets_with_image = pick_tweets_with_image
   end
 
   private
@@ -13,6 +18,7 @@ class ListTimelineController < ApplicationController
   def pick_tweets_with_image
     if (@tweets.is_a?(Array))
       @tweets.select do |tweet|
+        # http~含み且つRT含まない。
         !!(tweet.text =~ /https?:\/\/t\.co\/\w{10}/) && !(tweet.text =~ /RT/)
       end
     else
